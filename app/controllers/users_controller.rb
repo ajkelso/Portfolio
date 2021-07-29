6d1c46d4a55b48dd6b860c1cfb5be7a5d6fcf2bc
ajkelso <ajkelso@gmail.com>
2021-07-29 14:21:20 -0400

    create sessions and sign In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+    
+    before_action :find_user
+    
 
-    def show
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
+
+    def find_user
         @user = User.find_by(id: params[:id])
     end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :stocks
   resources :folios
   resources :users
+
+  get '/signin', to: 'sessions#new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
