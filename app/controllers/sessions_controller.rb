349c87833a369b4a6215edc13b819991c97f5a45
ajkelso <ajkelso@gmail.com>
2021-07-29 14:32:55 -0400

    add sessons#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,9 @@ class SessionsController < ApplicationController
     def new
     
     end
+
+    def create 
+        byebug
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
 
   get '/signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
