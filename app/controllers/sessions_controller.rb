7c60ab7be45ead03dfaea8b51769c0cb4e9e1a5a
ajkelso <ajkelso@gmail.com>
2021-07-30 12:04:21 -0400

    build session#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,22 @@ class SessionsController < ApplicationController
 
     def create 
         byebug
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path
+        else
+            flash[:error] = "Incorrect username or password"
+            redirect_to signin_path
+        end
+
     end
-    
+
+    private
+
+    def session_params
+        params.permit(:username, :password)
+    end
+
+
 end
